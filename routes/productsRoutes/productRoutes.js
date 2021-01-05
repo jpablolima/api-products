9e9c41d3d06b17b96b10a4f23080f31389cfd27d
jpablolima <jp.pablolima@gmail.com>
2021-01-05 12:56:27 -0300

    search product

--- routes/productsRoutes/productRoutes.js
+++ routes/productsRoutes/productRoutes.js
@@ -1,33 +1,56 @@
 const express = require('express')
-const listProdutos = express.Router()
+const productRoute = express.Router()
 const fs = require('fs')
 
 
-listProdutos.get('/', (req, res) => {
+productRoute.get('/', (req, res) => {
     res.send('Home')
 })
 
-listProdutos.get('/produtos', (req, res) => {
+productRoute.get('/produtos', (req, res) => {
 
     const data = fs.readFileSync('./services/db.json')
     const productsJson = JSON.parse(data)
     res.send(productsJson)
 })
 
-listProdutos.post('/produto/aadnew', (req, res) => {
+productRoute.post('/produto/aadnew', (req, res) => {
     const data = fs.readFileSync('./services/db.json')
     const productsJson = JSON.parse(data)
     const bodyRequest = req.body
 
     productsJson.products.push(bodyRequest)
 
-
     fs.writeFileSync('./services/db.json', JSON.stringify(productsJson))
     res.send('Product insert success')
-
-    // console.log(bodyRequest)
 
 })
 
 
-module.exports = listProdutos;
+productRoute.get('/produto/:id', (req, res) => {
+    const idProduct = req.params.id;
+    let productSearch = ''
+
+    const data = fs.readFileSync('./services/db.json')
+    const productsJson = JSON.parse(data)
+    productsJson.products.map(index => {
+        if (index.id == idProduct) {
+            productSearch = index
+        }
+    })
+
+    if (productSearch != "") {
+        res.send(productSearch)
+    } else {
+        res.send('Product not found')
+    }
+
+})
+
+
+
+
+
+
+
+module.exports = productRoute;
